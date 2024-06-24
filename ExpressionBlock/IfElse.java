a8d9b83dac75cd003ebcc9a4218d16eff7ae91ca
justyouraddi <aadityakumawat111@gmail.com>
2024-06-25 00:19:01 +0530

    added if else

---
+++ ExpressionBlock/IfElse.java
@@ -0,0 +1,13 @@
+package ExpressionBlock;
+
+public class IfElse {
+    public static void main(String[] args) {
+        boolean primeSubscription = false;
+
+        if(primeSubscription){
+            System.out.println("Welcome to prime membership");
+        }else{
+            System.out.println("Welcome to non prime");
+        }
+    }
+}
